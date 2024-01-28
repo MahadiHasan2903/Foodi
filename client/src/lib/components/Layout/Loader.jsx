a6de83bcc5a5e991546a491f0b30d47fe72516bc
MD. Mahadi Hasan <12mahadihasan@gmail.com>
2024-01-28 21:59:05 +0600

    updated

--- client/src/lib/components/Layout/Loader.jsx
+++ client/src/lib/components/Layout/Loader.jsx
@@ -4,7 +4,10 @@ import { HashLoader } from "react-spinners";
 const Loader = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <HashLoader color="#ffffff" />
+      <HashLoader
+        color="#ff0000"
+        className="text-primary w-[300px] h-[300pc]"
+      />
     </div>
   );
 };
